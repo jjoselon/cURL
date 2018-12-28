043ff5c920f17e520f6bcad3b15ea4fcd3204081
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2018-12-28 08:44:50 -0500

    Added sql util
    code happing "!

---
+++ SQL_Util.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `table_name`
+ADD COLUMN `name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL COMMENT 'comments are important',
+ADD COLUMN `name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL COMMENT 'comments are important',

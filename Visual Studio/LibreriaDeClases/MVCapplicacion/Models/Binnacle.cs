b40a9511c48d847c0e5d1b9310494acfb7c8bba5
José Ramirez <jjoselon3167@hotmail.com>
2019-04-08 12:29:24 -0500

    fin tutorial mvc

--- Visual Studio/LibreriaDeClases/MVCapplicacion/Models/Binnacle.cs
+++ Visual Studio/LibreriaDeClases/MVCapplicacion/Models/Binnacle.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCapplicacion.Models
 {
     public class Binnacle
     {
         public int Id { get; set; }
+        [StringLength(20)]
+        [Required]
         public string Title { get; set; }
+        [StringLength(20, MinimumLength = 5)]
+        [Required]
         public string Body { get; set; }
         public string BinnacleType { get; set; }
         public DateTime PublishDate { get; set; }

14026ca38575b5bf9575052e1c7107a99cab793b
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2019-04-08 21:26:50 -0500

    Add files via upload

---
+++ Visual Studio/MySqlDesktopApplication/MySqlDesktopApplication/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MySqlDesktopApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlSelect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sqlSelect
+            // 
+            this.sqlSelect.Location = new System.Drawing.Point(226, 175);
+            this.sqlSelect.Name = "sqlSelect";
+            this.sqlSelect.Size = new System.Drawing.Size(29, 25);
+            this.sqlSelect.TabIndex = 1;
+            this.sqlSelect.Text = "R";
+            this.sqlSelect.UseVisualStyleBackColor = true;
+            this.sqlSelect.Click += new System.EventHandler(this.sqlSelect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(191, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.sqlCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sqlSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sqlSelect;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Visual Studio/MySqlDesktopApplication/MySqlDesktopApplication/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+
+namespace MySqlDesktopApplication
+{
+    public partial class Form1 : Form
+    {
+        static string ConnectionString = "server=127.0.0.1;uid=root;pwd='';database=testconnectornet";
+        MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(ConnectionString);
+
+        public Form1()
+        {
+            InitializeComponent();
+            OpenConnection();
+        }
+
+        MySql.Data.MySqlClient.MySqlConnection OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        {
+                            MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        }
+                        break;
+                    case 1045:
+                        {
+                            MessageBox.Show("Invalid username/password, please try again");
+                        }
+                        break;
+                    default:
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                        break;
+                }
+            }
+            return conn;
+        }
+        void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        /**
+         * El objeto MySql.Data.MySqlClient.MySqlDataReader es usado para recibir datos de la base de datos 
+         */
+        private void sqlSelect_Click(object sender, EventArgs e)
+        {
+            
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand();
+            cmd.CommandText = "SELECT * FROM usuarios";
+            cmd.Connection = conn;
+            //cmd.CommandType = CommandType.TableDirect;
+            MySql.Data.MySqlClient.MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString(0));
+            }
+            // Es necesario cerrar el reader ya que solo puede haber asociado un reader a una connexión
+            reader.Close();
+        }
+
+        private void sqlCreate_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand();
+            cmd.CommandText = "INSERT INTO usuarios(nickname)";
+            cmd.Connection = conn;
+            //cmd.CommandType = CommandType.TableDirect;
+            MySql.Data.MySqlClient.MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(reader.GetString(0));
+            }
+        }
+    }
+}

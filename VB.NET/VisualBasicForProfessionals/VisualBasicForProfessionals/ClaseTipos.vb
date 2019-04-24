43b270a6531c669a98b8233130deab82ef76b585
José Alejandro Ramirez Rivera <jjoselon3167@hotmail.com>
2019-04-23 21:15:41 -0500

    Agrega proyecto VisualBasicForProfessionals
    Este proyecto sigue el libro Visual Basic para profesionales

---
+++ VB.NET/VisualBasicForProfessionals/VisualBasicForProfessionals/ClaseTipos.vb
@@ -0,0 +1,9 @@
+﻿Public Class ClaseTipos
+
+    Function IncrementCounter() As Integer
+
+        Return 3
+
+    End Function
+
+End Class
---
+++ VB.NET/VisualBasicForProfessionals/VisualBasicForProfessionals/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Public counter As Integer
+    Private _name As Integer
+
+    'Public [Dim] counter As Integer
+
+    Public Sub ExampleSub()
+        Static exampleStaticLocalVariable As Integer = 0
+        Console.WriteLine(exampleStaticLocalVariable.ToString)
+        exampleStaticLocalVariable += 2
+    End Sub
+
+    Sub Main()
+        ExampleSub()
+        ExampleSub()
+        ExampleSub()
+        Console.ReadKey()
+
+    End Sub
+
+    ' También podemos declarar clases dentro de un Module
+    Public Class ExampleClass
+
+    End Class
+
+End Module
